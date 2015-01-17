a656a5c36a908f9f1d4701b05b396d90c1103cbe
Kim <kimseongrim@gmail.com>
2015-01-17 22:28:18 +0800

    add UILabel & UIButton

---
+++ UILabel/UILabel/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  UILabel
+//
+//  Created by 金成林 on 15/1/17.
+//  Copyright (c) 2015年 Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
