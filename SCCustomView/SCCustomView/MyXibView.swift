5244417c248ebcf579641115c7fc5d292a30d741
Kim <kimseongrim@gmail.com>
2015-01-27 21:35:13 +0800

    customView is Good Good！

--- SCCustomView/SCCustomView/MyXibView.swift
+++ SCCustomView/SCCustomView/MyXibView.swift
@@ -12,25 +12,36 @@ import UIKit
 把这个类绑定到MyXibView.xib中的View的Custom class里面
 */
 class MyXibView: UIView {
+    
+    @IBOutlet weak var mButton: UIButton!
 
-    func addMyView() {
+    func updateMyView() {
         let mUITextField = UITextField()
         mUITextField.frame = CGRectMake(0,0,50,20)
         mUITextField.borderStyle = UITextBorderStyle.RoundedRect
         //        mUITextField.enabled = false
         self.addSubview(mUITextField)
+        
     }
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        addMyView()
+        updateMyView()
         
     }
 
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         
-        addMyView()
+        updateMyView()
     }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        //因为mButton直接存在于Xib（Nib）文件中，所以无法在init函数中对他做操作，只能在awakeFromNib中操作
+        mButton.setTitle("hello world!", forState: UIControlState.Normal)
+        
+    }
+    
 
 }
